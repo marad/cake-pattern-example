5bbd5f4862ae5d2e997f63b0b0d2f6ecc7b5b9d9
Marcin Radoszewski <moriturius@gmail.com>
2015-11-10 11:08:54 +0000

    Initial commit

---
+++ CakePattern.scala
@@ -0,0 +1,49 @@
+case class Flour()
+case class Bread(flour: Flour)
+
+// Component definitions
+trait MillComponent {
+  def mill: Mill
+
+  trait Mill {
+    def produceFlour(): Flour
+  }
+}
+
+trait BakeryComponent {
+  def bakery: Bakery
+
+  trait Bakery {
+    def bakeBread(): Bread
+  }
+}
+
+// Component implementations
+trait RegularMillComponent extends MillComponent {
+  def mill: Mill = new RegularMill
+
+  class RegularMill extends Mill {
+    def produceFlour() = Flour()
+  }
+}
+
+trait RegularBakeryComponent extends BakeryComponent {
+  this: MillComponent =>
+
+  def bakery: Bakery = new RegularBakery
+
+  class RegularBakery extends Bakery {
+    def bakeBread() = Bread(mill.produceFlour())
+  }
+}
+
+// Putting things together
+object Application extends App {
+  val app = new Object
+    with RegularMillComponent
+    with RegularBakeryComponent
+
+  val bread = app.bakery.bakeBread()
+
+  println(s"Baked bread: $bread")
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Cake Pattern Example
+
+This project is example for cake pattern as a dependency management tool.
+
+## Running
+
+To run this you will need SBT. That's the hard part. When ready then just run:
+
+    sbt run
